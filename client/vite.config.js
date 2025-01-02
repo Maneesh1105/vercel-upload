fc0c9e13d49636a21e252ff8d11f4ae454d1278f
maneesh1105 <maneeshsharma1105@gmail.com>
2025-01-02 23:02:41 +0530

    first commit

---
+++ client/vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: 'dist', // Output folder for production build
+  },
+  server: {
+    port: 4000, // Local development server port
+  },
+  resolve: {
+    alias: {
+      '@': '/src', // Shortcut for src folder
+    },
+  },
+});
